3a9ccbd56e376486344b344df5f956ba2eadd237
roopeshtripathi <roopesh.mobiledev@gmail.com>
2017-04-19 22:09:22 +0530

    Implemented stored & computed properties

--- StoredAndComputedProperties.playground/Contents.swift
+++ StoredAndComputedProperties.playground/Contents.swift
@@ -2,5 +2,43 @@
 
 import UIKit
 
-// Stored Properties
+
+class Thermometer {
+    
+    // Here farenheit is Stored Property
+
+    var farenheit : Double
+    
+    /* Here celsius is Computed Property
+        celsius is being computed from farenheit
+    */
+    var celsius : Double{
+        
+        get {
+            
+            return (farenheit - 32) * (5/9)
+        }
+        
+        set {
+            
+            farenheit = newValue * (9/5) + 32
+        
+        }
+    }
+    
+    init (farenheit : Double){
+        
+        self.farenheit = farenheit
+    }
+    
+    
+
+}
+
+let thermometer = Thermometer(farenheit: 502)
+thermometer.farenheit
+thermometer.celsius
+
+thermometer.celsius = 36
+thermometer.farenheit
 
